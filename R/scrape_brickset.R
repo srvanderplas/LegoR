ab92c6316bd8b77146837413bb5ddb282aad46ed
Susan Vanderplas <srvanderplas@gmail.com>
2019-08-07 17:28:44 -0500

    brickset api and v8 attempts

--- R/scrape_brickset.R
+++ R/scrape_brickset.R
@@ -8,23 +8,50 @@
 #' @examples
 #' library(Lego)
 #' themes <- brickset_get_themes()
-brickset_get_themes <- function(url) {
+brickset_get_themes <- function(url = "https://brickset.com/browse/sets") {
   doc <- xml2::read_html(url)
-  
+
   tibble::tibble(
     name = rvest::html_nodes(doc, ".navrow li a") %>% rvest::html_text(),
     link = rvest::html_nodes(doc, ".navrow li a") %>% rvest::html_attr("href") %>%
-      paste0("https://brickset.com", .), 
-    year = stringr::str_extract(link, "year-\\d{1,}") %>% 
-      stringr::str_remove("year-") %>% 
+      paste0("https://brickset.com", .),
+    year = stringr::str_extract(link, "year-\\d{1,}") %>%
+      stringr::str_remove("year-") %>%
       readr::parse_number()
   )
 }
 
 brickset_get_sets <- function(url) {
-  doc <- xml2::read_html(url)
+  doc_orig <- xml2::read_html(url)
+  ct <- V8::v8() # create a new V8 context
 
-  # tibble::tibble(
-  #   # set_number = rvest::
-  # )
+  # js to set csvview class
+  set_csvview <- "var element = document.getElementsByTagName('body')[0];
+                  element.classList.replace('listview', 'csvview');"
+
+
+  doc <- xml2::read_html(ct$eval(url)
+
+  sets <- rvest::html_nodes(doc, ".meta")
+  tibble::tibble(
+    # set_number = rvest::
+  )
 }
+
+#Loading both the required libraries
+library(rvest)
+library(V8)
+
+httr::POST(url = "https://brickset.com/api/v2.asmx/getSets",
+           body = "apiKey=xxx&theme=ArchitecturepageSize=500")
+
+#URL with js-rendered content to be scraped
+link <- "https://brickset.com/sets/theme-Architecture/year-2019"
+#Read the html page content and extract all javascript codes that are inside a list
+emailjs <- read_html(link) %>% html_nodes('li') %>% html_nodes('script') %>% html_text()
+# Create a new v8 context
+ct <- V8::v8()
+#parse the html content from the js output and print it as text
+read_html(ct$eval(gsub('document.write','',emailjs))$source(link)) %>%
+  html_text()
+info@brewhemia.co.uk
